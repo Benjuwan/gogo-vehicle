f68b18f607942429bff1fd6755d967623c4a1b45
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2025-05-28 16:30:16 +0900

    first upload.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Drawer } from "./components/Drawer";
+
+export const App = () => {
+  return (
+    <>
+      <h1 className="text-center leading-[2em]">GoGo Vehicle</h1>
+      <Drawer />
+    </>
+  );
+}
